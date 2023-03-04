20a613580e59fa6aaa6bdfcd6e6c884e9306eaa4
AlexShkn <barsuk-b@yandex.ru>
2023-03-04 23:21:20 +0300

    section reviews +adaptive

--- src/js/components/swiper/slider.js
+++ src/js/components/swiper/slider.js
@@ -24,9 +24,9 @@ const clientsSlider = new Swiper('.clients-slider', {
 	},
 })
 
-const workBulletStart = document.querySelector('[data-work-bullet-start]')
-const workBulletMiddle = document.querySelector('[data-work-bullet-middle]')
-const workBulletEnd = document.querySelector('[data-work-bullet-end]')
+const worksBulletStart = document.querySelector('[data-works-bullet-start]')
+const worksBulletMiddle = document.querySelector('[data-works-bullet-middle]')
+const worksBulletEnd = document.querySelector('[data-works-bullet-end]')
 
 const worksSlider = new Swiper('.works-slider', {
 	observer: true,
@@ -53,19 +53,67 @@ const worksSlider = new Swiper('.works-slider', {
 	on: {
 		//Если слайд последний
 		reachEnd: () => {
-			workBulletEnd.classList.add('_active')
-			workBulletMiddle.classList.remove('_active')
+			worksBulletEnd.classList.add('_active')
+			worksBulletMiddle.classList.remove('_active')
 		},
 		//Если слайд первый
 		reachBeginning: () => {
-			workBulletStart.classList.add('_active')
-			workBulletMiddle.classList.remove('_active')
+			worksBulletStart.classList.add('_active')
+			worksBulletMiddle.classList.remove('_active')
 		},
 		// Если не начальная и не конечная позиция
 		fromEdge: () => {
-			workBulletStart.classList.remove('_active')
-			workBulletEnd.classList.remove('_active')
-			workBulletMiddle.classList.add('_active')
+			worksBulletStart.classList.remove('_active')
+			worksBulletEnd.classList.remove('_active')
+			worksBulletMiddle.classList.add('_active')
+		},
+	},
+})
+
+const reviewsBulletStart = document.querySelector('[data-reviews-bullet-start]')
+const reviewsBulletMiddle = document.querySelector(
+	'[data-reviews-bullet-middle]',
+)
+const reviewsBulletEnd = document.querySelector('[data-reviews-bullet-end]')
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+	observer: true,
+	observeParents: true,
+	spaceBetween: 35,
+	navigation: {
+		nextEl: '.reviews-slider-next',
+		prevEl: '.reviews-slider-prev',
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		768: {
+			spaceBetween: 10,
+			slidesPerView: 2,
+		},
+		991: {
+			spaceBetween: 35,
+			slidesPerView: 3,
+		},
+	},
+
+	on: {
+		//Если слайд последний
+		reachEnd: () => {
+			reviewsBulletEnd.classList.add('_active')
+			reviewsBulletMiddle.classList.remove('_active')
+		},
+		//Если слайд первый
+		reachBeginning: () => {
+			reviewsBulletStart.classList.add('_active')
+			reviewsBulletMiddle.classList.remove('_active')
+		},
+		// Если не начальная и не конечная позиция
+		fromEdge: () => {
+			reviewsBulletStart.classList.remove('_active')
+			reviewsBulletEnd.classList.remove('_active')
+			reviewsBulletMiddle.classList.add('_active')
 		},
 	},
 })
